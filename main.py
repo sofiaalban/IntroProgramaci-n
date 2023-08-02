91925f0f2dd29b1fc78535b9f6cb9ad93178ad1d
sofiaalban <141078625+sofiaalban@users.noreply.github.com>
2023-08-02 23:26:20 +0000

    ej8

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+'''
 #ejercicio1
 print("Hola Mundo")
 cadena = "Hola Mundo"
@@ -9,3 +10,28 @@ print("hola"+" "+nombre)
 
 #ejercicio4
 print(((3+2)/(2*5))**2)
+
+#ejercicio5
+horas= float(input("número de horas trabajadas "))
+pago= float(input("costo de la hora "))
+print(pago*horas)
+
+#ejercicio6
+n=int(input("número "))
+suma=(n*(n+1)/2)
+print(suma)
+
+#ejercicio7
+peso=float(input("peso en kg "))
+estatura=float(input("peso en metros "))
+imc=(peso/estatura**2)
+print(round(imc,2))
+'''
+
+#ejercicio8
+num1=int(input("dato 1 "))
+num2=int(input("dato 2 "))
+cociente=(num1//num2)
+print(cociente)
+resto=(num1%num2)
+print(resto)
