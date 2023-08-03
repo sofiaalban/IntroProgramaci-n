6f59e4030cde0a526c0dca5b83f19f155466e688
sofiaalban <141078625+sofiaalban@users.noreply.github.com>
2023-08-03 00:00:22 +0000

    10

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ peso=float(input("peso en kg "))
 estatura=float(input("peso en metros "))
 imc=(peso/estatura**2)
 print(round(imc,2))
-'''
+
 
 #ejercicio8
 num1=int(input("dato 1 "))
@@ -35,3 +35,16 @@ cociente=(num1//num2)
 print(cociente)
 resto=(num1%num2)
 print(resto)
+
+#ejercicio9
+c=float(input("cantidad a invertir "))
+i=float(input("tasa de interés anual "))
+año=float(input("número de años  "))
+cap=(c*(i/100+1)**año)
+print(round(cap,2))
+'''
+#ejercicio10
+payasos=float(input("número de payasos "))
+muñecas=float(input("número de muñecas "))
+total=(payasos*112)+(muñecas*75)
+print("el peso total es ", total)
