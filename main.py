d1ca47ab9333c9749457e8d9cdffc07ef9c2beb6
sofiaalban <141078625+sofiaalban@users.noreply.github.com>
2023-08-14 21:36:38 +0000

    .

--- main.py
+++ main.py
@@ -42,9 +42,33 @@ i=float(input("tasa de interés anual "))
 año=float(input("número de años  "))
 cap=(c*(i/100+1)**año)
 print(round(cap,2))
-'''
+
 #ejercicio10
 payasos=float(input("número de payasos "))
 muñecas=float(input("número de muñecas "))
 total=(payasos*112)+(muñecas*75)
 print("el peso total es ", total)
+
+#2
+num1=int(input("dato 1 "))
+num2=int(input("dato 2 "))
+cociente=(num1//num2)
+print(cociente)
+resto=(num1%num2)
+print(resto)
+if c<1:
+  print("El divisor es mayor que el dividendo")
+else
+  print("El divisor es mayor que el divisor")
+'''
+#ejercicio3
+inv=float(input("ingresa la cantidad a invertir"))
+int=float(input("ingresa el interes anual"))
+años=float(input("ingresa el número de años: "))
+fórmula=(inv*((int/100)+1)**años)
+if formula <100000:
+  print("baja rentabilidad")
+elif 100000<formula>100000:
+  print("rentabilidad baja")
+else:
+  print("es una buena inversión") 
